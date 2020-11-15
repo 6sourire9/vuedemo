466975b53302506d841911843221f5a6f832138c
6sourire9 <859672003@qq.com>
2020-11-15 19:28:43 +0800

    项目

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,19 @@
   <!--  <HelloWorld>Welcome to Your Vue.js</HelloWorld>-->
   <!--  <TodoList />-->
   <Home />
+  <TabBar />
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TodoList from "@/components/todolist/TodoList";
 import Home from "./pages/Home";
+import TabBar from "./components/tabbar/TabBar";
 
 export default {
   name: "App",
   components: {
+    TabBar,
     Home
     // TodoList
     // HelloWorld
--- src/components/tabbar/TabBar.vue
+++ src/components/tabbar/TabBar.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <img />
-    <p></p>
+  <div class="tabBarBox">
+    <div class="smallBox" v-for="i in tabData" :key="i.id">
+      <img />
+      <span>{{ i.name }}</span>
+    </div>
   </div>
 </template>
 
@@ -12,7 +14,20 @@ export default {
     return {
       tabData: [
         {
+          id: 1,
           name: "首页"
+        },
+        {
+          id: 2,
+          name: "购物车"
+        },
+        {
+          id: 3,
+          name: "我的"
+        },
+        {
+          id: 4,
+          name: "商品"
         }
       ]
     };
@@ -20,4 +35,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.tabBarBox {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+}
+.smallBox {
+  /*display: inline-block;*/
+}
+</style>
